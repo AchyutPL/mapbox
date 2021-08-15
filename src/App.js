6c1ba89ee0dd8d8d6f402c5cabe897efc27b5074
Achyut <achyutpaudel24@gmail.com>
2021-08-15 19:29:04 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+/** @format */
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 700,
+    height: 500,
+    latitude: 27.708727,
+    longitude: 85.316536,
+    zoom: 15,
+    pitch: 50,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Mapbox Demo</h1>
+
+      <ReactMapGL
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxApiAccessToken={
+          "pk.eyJ1IjoiYWNoeXV0cGF1ZGVsIiwiYSI6ImNrc2E2dzA5MDBmN2kydW55cWJkdWhxanEifQ.f9qNFGsdO5gFMvcpvjiMjw"
+        }
+        {...viewport}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}>
+        <Marker
+          latitude={27.708727}
+          longitude={85.316536}
+          offsetLeft={-20}
+          offsetTop={-10}>
+          <img src="pin.png" style={{ height: "40px" }} alt="img" />
+        </Marker>
+        <Marker
+          latitude={27.708729}
+          longitude={85.316536}
+          offsetLeft={-90}
+          offsetTop={-10}>
+          <img src="pin.png" style={{ height: "40px" }} alt="img" />
+        </Marker>
+      </ReactMapGL>
     </div>
   );
 }
